2555cdfe9774eaa700e117187b88ef1fbef203fc
Emmanuel2006c <62173368+Emmanuel2006bot@users.noreply.github.com>
2022-04-18 17:07:27 +0200

    kleine Änderung am KIllerListener

--- Game_Plugin/src/main/java/game/plugin/game_plugin/Listener/JoinListener.java
+++ Game_Plugin/src/main/java/game/plugin/game_plugin/Listener/JoinListener.java
@@ -4,9 +4,6 @@ import game.plugin.game_plugin.scoreboard.TestScoreboard;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 
@@ -20,25 +17,5 @@ public class JoinListener implements Listener {
 
     }
 
-    @EventHandler()
-    public void onPlayerDeath( final EntityDeathEvent event) {
-        if (event.getEntity() instanceof Player) {
-
-            Player player = (Player) event.getEntity();
-            EntityDamageEvent deathcause = player.getLastDamageCause();
-            if (deathcause.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK) {
-                Entity entity = (((EntityDamageByEntityEvent)deathcause).getDamager());
-
-
-
-                if(entity instanceof Player){
-                    Player killerPlayer = (Player)entity;
-                    player.sendMessage("Du wurdest von " +killerPlayer.getName() + ( "getötet"));
-
-                }
-            }
-
-        }
-
-    }
 }
+
