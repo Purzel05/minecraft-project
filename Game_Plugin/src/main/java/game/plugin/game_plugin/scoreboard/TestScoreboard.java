3ad17f48ced3842eaafd53050363638009ab937d
Emmanuel2006c <62173368+Emmanuel2006bot@users.noreply.github.com>
2022-04-19 22:08:16 +0200

    Killcounter

--- Game_Plugin/src/main/java/game/plugin/game_plugin/scoreboard/TestScoreboard.java
+++ Game_Plugin/src/main/java/game/plugin/game_plugin/scoreboard/TestScoreboard.java
@@ -2,7 +2,7 @@ package game.plugin.game_plugin.scoreboard;
 import org.bukkit.entity.Player;
 
 public class TestScoreboard extends scorboardbuilder {
-
+    int content = 0;
     public TestScoreboard(Player player) {
 
         super(player, "  §5§[Killcounter  ");
@@ -11,17 +11,19 @@ public class TestScoreboard extends scorboardbuilder {
 
     @Override
     public void createScoreboard() {
-
-        setScore("Leo stinkt",0);
-
+        setScore(0,0);
      }
 
 
     @Override
     public void update() {
-
         
+    }
 
+
+    public void countScore(){
+        this.scoreboard.getScores(String.valueOf(content));
+        this.content = content + 1;
     }
 
 
