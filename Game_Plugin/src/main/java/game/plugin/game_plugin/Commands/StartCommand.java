412c4d6752a60c52f5d5166f0cfea8eab0cc39fd
Porzl2k5 <leo.prentl@gmail.com>
2022-04-21 13:31:09 +0200

    PluginYML StartCommand registration

--- Game_Plugin/src/main/java/game/plugin/game_plugin/Commands/StartCommand.java
+++ Game_Plugin/src/main/java/game/plugin/game_plugin/Commands/StartCommand.java
@@ -12,16 +12,14 @@ public class StartCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        Plugin lobby_plugin = Bukkit.getPluginManager().getPlugin("Lobby_Plugin");
+        Plugin lobby_Plugin = Bukkit.getPluginManager().getPlugin("Lobby_Plugin");
 
-        if(lobby_plugin.isEnabled()) {
+            if(lobby_Plugin.isEnabled()) {
 
-            Bukkit.getPluginManager().disablePlugin(lobby_plugin);
-            Bukkit.getLogger().fine("Lobby_Plugin wird deaktiviert.");
+                Bukkit.getPluginManager().disablePlugin(lobby_Plugin);
+                Bukkit.getLogger().fine("Lobby_Plugin wird deaktiviert.");
 
-        }
-
-
+            }
 
         return false;
     }
