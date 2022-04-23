37813ac71c71f58ebda7792b5162ed95ecbdb81f
Porzl2k5 <leo.prentl@gmail.com>
2022-04-23 22:53:06 +0200

    LittleChanges2

--- Game_Plugin/src/main/java/game/plugin/game_plugin/scoreboard/ScoarboardBuilder.java
+++ Game_Plugin/src/main/java/game/plugin/game_plugin/scoreboard/ScoarboardBuilder.java
@@ -58,4 +58,4 @@ public abstract class ScoarboardBuilder {
 
     }
 
-    }
+}
--- Game_Plugin/src/main/java/game/plugin/game_plugin/scoreboard/TestScoreboard.java
+++ Game_Plugin/src/main/java/game/plugin/game_plugin/scoreboard/TestScoreboard.java
@@ -1,13 +1,8 @@
 package game.plugin.game_plugin.scoreboard;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityDeathEvent;
 
-public class TestScoreboard extends ScoarboardBuilder implements Listener {
+import org.bukkit.entity.Player;
+
+public class TestScoreboard extends ScoarboardBuilder {
     private int content = 0;
     private Player killerPlayer;
 
@@ -16,56 +11,26 @@ public class TestScoreboard extends ScoarboardBuilder implements Listener {
         super(player, "  §5§[Killcounter  ");
 
     }
+
     @Override
     public void createScoreboard() {
 
-        setScore(String.valueOf(content),0);
-
-     }
-     @Override
-    public void update() {
-
-    }
-
-    @EventHandler
-    public void onPlayerDeath(final EntityDeathEvent event) {
-        if (event.getEntity() instanceof Player) {
-
-            Player player = (Player) event.getEntity();
-            EntityDamageEvent deathcause = player.getLastDamageCause();
-
-            if (deathcause.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK) {
-
-                Entity entity = (((EntityDamageByEntityEvent) deathcause).getDamager());
-
-                if (entity instanceof Player) {
-
-                    Player killerPlayer = (Player) entity;
-                    player.sendMessage("§5§l§[Du wurdest von " + killerPlayer.getName() + ("§5§l§[ und dem allmächtigem Schleggagott zerquetscht"));
-
-                    this.killerPlayer = event.getEntity().getKiller();
-
-                    killUpdate();
-
-                }
-            }
-
-        }
+        setScore(String.valueOf(content), 0);
 
     }
 
     public void killUpdate() {
 
-        this.content = content + 1;
+        content = content + 1;
+        setScore(String.valueOf(content), 0);
 
-        TestScoreboard killerScoarboard = new TestScoreboard(killerPlayer);
+    }
 
-        killerScoarboard.setScore(String.valueOf(content), 0);
+    @Override
+    public void update() {
 
     }
 
 }
-
-
 
 
