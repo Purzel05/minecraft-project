3c4da137ef9e0d22745d0742c677af84200d861a
Porzl2k5 <leo.prentl@gmail.com>
2022-04-24 21:06:19 +0200

    DeathMessage

--- Game_Plugin/src/main/java/game/plugin/game_plugin/Listener/PlayerDeathListener.java
+++ Game_Plugin/src/main/java/game/plugin/game_plugin/Listener/PlayerDeathListener.java
@@ -1,5 +1,6 @@
 package game.plugin.game_plugin.Listener;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,7 +14,12 @@ public class PlayerDeathListener implements Listener {
     public void onDeath(PlayerDeathEvent event) {
 
         Player player = event.getEntity().getPlayer();
-        event.setDeathMessage(ChatColor.GOLD + player.getName() + ChatColor.DARK_RED + " hats erwischt.");
+
+        for(Player p : Bukkit.getOnlinePlayers()) {
+            if(p.getName() != player.getName()) {
+                p.sendMessage(ChatColor.GOLD + event.getEntity().getPlayer().getName() + ChatColor.DARK_RED + " hats erwischt");
+            }
+        }
 
     }
 
