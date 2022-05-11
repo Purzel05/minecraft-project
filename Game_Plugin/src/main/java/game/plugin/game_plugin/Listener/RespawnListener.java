880bd09ff7e71f95b19ee71d0d4d64b03de4a149
Emmanuel2006c <62173368+Emmanuel2006bot@users.noreply.github.com>
2022-05-11 21:01:48 +0200

    HHHHHHA

--- Game_Plugin/src/main/java/game/plugin/game_plugin/Listener/RespawnListener.java
+++ Game_Plugin/src/main/java/game/plugin/game_plugin/Listener/RespawnListener.java
@@ -4,9 +4,18 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.plugin.Plugin;
+
+import java.net.DatagramPacket;
 
 public class RespawnListener implements Listener {
+
+    public static Plugin plugin;
+    public void Event (Plugin plugin){
+        this.plugin = plugin;
+    }
 
     public void onRespawn(PlayerRespawnEvent event) {
         Player player = event.getPlayer();
@@ -14,4 +23,18 @@ public class RespawnListener implements Listener {
         player.teleport(respawnLocation);
     }
 
+    public void onDeath(PlayerDeathEvent event){
+        Player player = event.getEntity();
+        Bukkit.getScheduler().runTaskLater(this.plugin, new Runnable() {
+
+            @Override
+            public void run() {
+             
+            }
+        },20);
+
+
+    }
+
 }
+
