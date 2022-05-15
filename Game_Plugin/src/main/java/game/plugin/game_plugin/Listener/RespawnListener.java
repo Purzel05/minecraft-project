d08481bc750428f0aaa5cf63aa31719b821c18fc
Porzl2k5 <leo.prentl@gmail.com>
2022-05-15 21:35:13 +0200

    E-Manuel

--- Game_Plugin/src/main/java/game/plugin/game_plugin/Listener/RespawnListener.java
+++ Game_Plugin/src/main/java/game/plugin/game_plugin/Listener/RespawnListener.java
@@ -1,22 +1,15 @@
 package game.plugin.game_plugin.Listener;
 
+import game.plugin.game_plugin.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.plugin.Plugin;
 
-import java.net.DatagramPacket;
 
 public class RespawnListener implements Listener {
-
-    public static Plugin plugin;
-    public void Event (Plugin plugin){
-        this.plugin = plugin;
-    }
-
     public void onRespawn(PlayerRespawnEvent event) {
         Player player = event.getPlayer();
         Location respawnLocation = new Location(Bukkit.getWorld("world"), -392.5, 16, -13.5);
@@ -24,17 +17,17 @@ public class RespawnListener implements Listener {
         player.setHealth(20);
         player.setFoodLevel(20);
     }
+    public void onDeath(EntityDeathEvent event){
 
-    public void onDeath(PlayerDeathEvent event){
-        Player player = event.getEntity();
-        Bukkit.getScheduler().runTaskLater(this.plugin, new Runnable() {
-
-
-            @Override
-            public void run() {
-
-            }
+        if(event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            Bukkit.getScheduler().runTaskLater(Main.getPlugin(Main.class),new Runnable() {
+                @Override
+                public void run() {
+                    player.spigot().respawn();
+                }
+            }, 20);
         }
-
+    }
 }
 
