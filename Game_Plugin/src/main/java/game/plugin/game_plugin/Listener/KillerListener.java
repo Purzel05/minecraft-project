7eea612e62713d669d9447e8b7b4dd7357b19f76
Porzl2k5 <leo.prentl@gmail.com>
2022-05-16 22:29:54 +0200

    Improvements

--- Game_Plugin/src/main/java/game/plugin/game_plugin/Listener/KillerListener.java
+++ Game_Plugin/src/main/java/game/plugin/game_plugin/Listener/KillerListener.java
@@ -4,6 +4,7 @@ import game.plugin.game_plugin.Commands.StartCommand;
 import game.plugin.game_plugin.scoreboard.KillcounterScoreboard;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,6 +12,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class KillerListener implements Listener {
 
@@ -27,8 +29,12 @@ public class KillerListener implements Listener {
                     Player killerPlayer = (Player) killerEntity;
                     killedPlayer.sendMessage("§5§l§[Du wurdest von " + killerPlayer.getName() + ("§5§l§[ und dem allmächtigem Schleggagott zerquetscht"));
 
+                    ItemStack Arrows = new ItemStack(Material.ARROW, 32);
                     KillcounterScoreboard killerScoreboard = StartCommand.scoreboards.get(killerPlayer.getUniqueId());
                     killerScoreboard.killUpdate();
+                    killerPlayer.setHealth(20);
+                    killerPlayer.getInventory().remove(Material.ARROW);
+                    killerPlayer.getInventory().setItem(20,Arrows);
 
                     if(killerScoreboard.getContent() == 10) {
                         Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "end");
