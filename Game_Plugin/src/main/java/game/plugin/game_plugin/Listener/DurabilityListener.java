8161af8794e30b2eef5b4e40c07b8374f4ff6a5f
Porzl2k5 <leo.prentl@gmail.com>
2022-05-20 21:35:00 +0200

    FlintAndSteel

--- Game_Plugin/src/main/java/game/plugin/game_plugin/Listener/DurabilityListener.java
+++ Game_Plugin/src/main/java/game/plugin/game_plugin/Listener/DurabilityListener.java
@@ -1,15 +1,39 @@
 package game.plugin.game_plugin.Listener;
 
+import org.bukkit.*;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.PistonMoveReaction;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerItemDamageEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.util.BoundingBox;
+import org.bukkit.util.Vector;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 public class DurabilityListener implements Listener {
 
     @EventHandler
     public void onItemDamage(PlayerItemDamageEvent event) {
 
-        event.setCancelled(true);
+        Player player = event.getPlayer();
 
+        if(player.getInventory().getItemInMainHand().getType() == Material.FLINT_AND_STEEL) {
+            event.setCancelled(false);
+        }else {
+            event.setCancelled(true);
+        }
     }
 }
