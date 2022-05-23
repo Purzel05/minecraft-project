a6a10d8d063ac5da4910fb7715d3537a86035402
Porzl2k5 <leo.prentl@gmail.com>
2022-05-23 17:02:58 +0200

    FlintAndSteelWorking + 5ArrowsByDeath

--- Game_Plugin/src/main/java/game/plugin/game_plugin/Listener/KillerListener.java
+++ Game_Plugin/src/main/java/game/plugin/game_plugin/Listener/KillerListener.java
@@ -4,10 +4,8 @@ import game.plugin.game_plugin.Commands.StartCommand;
 import game.plugin.game_plugin.scoreboard.KillcounterScoreboard;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -21,24 +19,16 @@ public class KillerListener implements Listener {
         if (event.getEntity() instanceof Player) {
             Player killedPlayer = (Player) event.getEntity();
             EntityDamageEvent deathcause = killedPlayer.getLastDamageCause();
-
             if (deathcause.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK) {
                 Entity killerEntity = (((EntityDamageByEntityEvent) deathcause).getDamager());
-                if(killerEntity instanceof Projectile) {
-                    Projectile projectile = (Projectile) killerEntity;
-                    if(projectile.getShooter() instanceof Player){
-                        Player projectileSender = (Player) projectile.getShooter();
-                        killerEntity = projectileSender;
-                    }
-                }
                 if (killerEntity instanceof Player) {
                     Player killerPlayer = (Player) killerEntity;
                     killedPlayer.sendMessage("§5§l§[Du wurdest von " + killerPlayer.getName() + ("§5§l§[ und dem allmächtigem Schleggagott zerquetscht"));
-
+                    int arrowAmount = killerPlayer.getInventory().getItem(20).getAmount();
                     KillcounterScoreboard killerScoreboard = StartCommand.scoreboards.get(killerPlayer.getUniqueId());
                     killerScoreboard.killUpdate();
                     killerPlayer.setHealth(20);
-
+                    killerPlayer.getInventory().getItem(20).setAmount(arrowAmount + 5);
                     if(killerScoreboard.getContent() == 10) {
                         Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "end");
                         killerPlayer.sendMessage(ChatColor.GREEN + "Du hast gewonnen!");
