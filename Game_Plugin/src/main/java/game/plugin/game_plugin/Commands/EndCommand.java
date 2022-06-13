52409ecfa9c082a4dd23b8271f7040087df53a3b
Porzl2k5 <leo.prentl@gmail.com>
2022-06-13 16:04:31 +0200

    XxX

--- Game_Plugin/src/main/java/game/plugin/game_plugin/Commands/EndCommand.java
+++ Game_Plugin/src/main/java/game/plugin/game_plugin/Commands/EndCommand.java
@@ -10,7 +10,6 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.scoreboard.DisplaySlot;
 
 import java.util.List;
 
